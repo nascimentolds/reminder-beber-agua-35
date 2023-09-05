2eb1bc67bb760d3463d44ec17fa2e14463228c1b
Marcelo Nascimento <nascimentolds@hotmail.com>
2023-09-04 23:50:50 -0300

    Timer control

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 const metaDiaria = document.getElementById('meta-diaria');
+const quantidade = document.getElementById('quantidade');
 
 metaDiaria.addEventListener('input', () => {
-    console.log(metaDiaria.value)
-
-    const valor = metaDiaria.value
+    const total = metaDiaria.max
+    const atual = metaDiaria.value
+    var valor = (atual * 100) / total
+    valor = parseInt(valor)
     
     metaDiaria.style.background = `linear-gradient(to right, #7fc4ed ${valor}%, #322f40 ${valor}%, #322f40 ${25-valor}%, #322f40 ${50-valor}%, #322f40 ${100-valor}%)`;
+})
+
+quantidade.addEventListener('input', () => {
+    const total = quantidade.max
+    const atual = quantidade.value
+    var valor = (atual * 100) / total
+    valor = parseInt(valor)
+    
+    quantidade.style.background = `linear-gradient(to right, #7fc4ed ${valor}%, #322f40 ${valor}%, #322f40 ${25-valor}%, #322f40 ${50-valor}%, #322f40 ${100-valor}%)`;
 })
